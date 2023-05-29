d232dca94d8ca7a8d539f3b377c17e0acfc09360
fangez <quic_fangez@quicinc.com>
2023-05-29 18:06:57 +0800

    weston:fix retire fence issue in sdm-backend
    1.fix DP retire fence fd leak issue
    2.fix DP video playback fd leak issue
    
    retire_fence_fd would be overwrite before dup.
    so it would not be completed for previous layer stack.
    actually we need wait previous layer stack retire fence for every time.
    and sync_wait in vblank_cb is not necessary for frame timing.
    
    Change-Id: I028fea28f7afe8160ade46cafbc2b00da2760102

--- libweston/backend-sdm/sdm-service/sdm_display.cpp
+++ libweston/backend-sdm/sdm-service/sdm_display.cpp
@@ -216,7 +216,7 @@ void SdmDisplay::RefreshWithCachedLayerstack()
         DLOGE("Commit failed with error %d", error);
         return;
     } else{
-        PostCommit(&prev_output_->retire_fence_fd);
+        PostCommit();
         NotifyOnRefresh(prev_output_);
     }
 }
@@ -902,29 +902,24 @@ DisplayError SdmDisplay::PreCommit()
 }
 
 
-DisplayError SdmDisplay::PostCommit(int *retire_fence_fd)
+DisplayError SdmDisplay::PostCommit()
 {
     DisplayError error = kErrorNone;
+
+    //Wait for retire fence fds
+    if (prev_layer_stack_.retire_fence) {
+        int ret = -1;
+        ret = Fence::Wait(prev_layer_stack_.retire_fence);
+        if (ret != kErrorNone) {
+            DLOGE("retire_fence wait timeout! ret=%d\n", ret);
+        }
+    }
 
     prev_layer_stack_ = layer_stack_;
     //Iterate through the layer buffer and close release fences
     for (uint32_t i = 0; i < layer_stack_.layers.size(); i++) {
         Layer *layer = layer_stack_.layers.at(i);
         LayerBuffer *layer_buffer = &layer->input_buffer;
-    }
-
-    //Wait for release fence fds
-    if (layer_stack_.retire_fence) {
-
-      if((*retire_fence_fd) > 0) {
-        int ret = -1;
-        ret = Fence::Wait(layer_stack_.retire_fence);
-        DLOGD("Fence fd close real_pre(%d) pre(%d) ret(%d)\n",
-            (*retire_fence_fd), previous_retire_fence_fd_, ret);
-      }
-
-      *retire_fence_fd = previous_retire_fence_fd_;
-      previous_retire_fence_fd_ = Fence::Dup(layer_stack_.retire_fence);
     }
 
     return error;
@@ -957,7 +952,7 @@ DisplayError SdmDisplay::Commit(struct drm_output *output)
     prev_output_ = output;
     ret = display_intf_->Commit(&layer_stack_);
 
-    PostCommit(&output->retire_fence_fd);
+    PostCommit();
 
     DLOGV("success");
     return ret;
