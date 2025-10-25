2c64b74ebeeda8ee8a5af743bace0b5b2fe02ef0
Kai Xing <kxing@qti.qualcomm.com>
2025-10-25 14:25:23 +0800

    weston: No disable punchhole support for weston by pixman
    Change-Id: I930c874d56353ef90b081bfc10f60b24ed529f41
    Signed-off-by: Kai Xing <kxing@qti.qualcomm.com>

--- libweston/backend-sdm/sdm-service/sdm_display.cpp
+++ libweston/backend-sdm/sdm-service/sdm_display.cpp
@@ -145,6 +145,7 @@ DisplayError SdmDisplay::CreateDisplay(uint32_t display_id) {
     DisplayError error = kErrorNone;
     char property[MAX_PROP_STR_SIZE] = {0};
     struct DisplayHdrInfo display_hdr_info = {};
+    int gpu_fd;
 
     error = core_intf_->CreateDisplay(display_type_, this, &display_intf_);
 
@@ -189,13 +190,17 @@ DisplayError SdmDisplay::CreateDisplay(uint32_t display_id) {
 
     frame_dumper_ = new SdmFrameDumper(display_id, GetDisplayString(), buffer_allocator_);
 
-    std::bitset<kClientCapabilityMax> client_capabilities =
+    gpu_fd = open("/dev/kgsl-3d0", O_RDWR);
+    if (gpu_fd >= 0) {
+	std::bitset<kClientCapabilityMax> client_capabilities =
                                  std::bitset<kClientCapabilityMax>().set();
-    client_capabilities.reset(kPunchholeSupported);
+	client_capabilities.reset(kPunchholeSupported);
 
-    error = display_intf_->SetClientTargetCapability(client_capabilities);
-    if (error != kErrorNone) {
-        DLOGW("Failed to populate client capabilities");
+	error = display_intf_->SetClientTargetCapability(client_capabilities);
+	if (error != kErrorNone) {
+	    DLOGW("Failed to populate client capabilities");
+	}
+	close(gpu_fd);
     }
 
     return kErrorNone;
